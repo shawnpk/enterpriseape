78a807fbf66ef847a7526788c645ae7e6e9466ae
Roy Bakker <roybakker17@gmail.com>
2015-01-19 14:03:16 +0000

    built out navigation and added welcome controller, set root

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contac
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+end
