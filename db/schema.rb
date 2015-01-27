ef0e5ba3856e4660f97e3c7c07fd1739b36719f2
Roy Bakker <roybakker17@gmail.com>
2015-01-27 14:07:44 +0000

    Add selection

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122130732) do
+ActiveRecord::Schema.define(version: 20150122162018) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150122130732) do
     t.string   "salesperson"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status_type"
   end
 
   create_table "users", force: true do |t|
