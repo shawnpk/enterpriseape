ea583613616075b4ad62084b9e598285fe375612
Roy Bakker <roybakker17@gmail.com>
2015-01-27 20:30:41 +0000

    Adding Items to Nested Attributes

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -1,47 +1,131 @@
-class PurchasesController < ApplicationController
-  before_action :set_purchase, only: [:show, :edit, :update, :destroy]
+class Invoices::PurchasesController < ApplicationController
 
-  respond_to :html
-
+  # GET /purchases
+  # GET /purchases.json
   def index
     @purchases = Purchase.all
-    respond_with(@purchases)
   end
 
+  # GET /purchases/1
+  # GET /purchases/1.json
   def show
-    respond_with(@purchase)
   end
 
+  # GET /purchases/new
   def new
+    @invoice = Invoice.find(params[:invoice_id])
     @purchase = Purchase.new
-    respond_with(@purchase)
   end
 
+  # GET /purchases/1/edit
   def edit
   end
 
+  # POST /purchases
+  # POST /purchases.json
   def create
+    @invoice = Invoice.find(params[:invoice_id])
     @purchase = Purchase.new(purchase_params)
-    @purchase.save
-    respond_with(@purchase)
+    @purchase.invoice = @invoice
+
+    respond_to do |format|
+      if @purchase.save
+        format.html { redirect_to @invoice, notice: 'Purchase was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @invoice }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /purchases/1
+  # PATCH/PUT /purchases/1.json
   def update
-    @purchase.update(purchase_params)
-    respond_with(@purchase)
+    respond_to do |format|
+      if @purchase.update(purchase_params)
+        format.html { redirect_to @purchase, notice: 'Purchase was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @purchase.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /purchases/1
+  # DELETE /purchases/1.json
   def destroy
-    @purchase.destroy
-    respond_with(@purchase)
+    @invoice = Invoice.find(params[:invoice_id])
+    @purchase = Purchase.find(params[:id])
+    title = @purchase.name
+    
+    if @purchase.destroy
+      flash[:notice] = "#{title} was deleted successfully."
+      redirect_to @invoice
+    else
+      flash[:error] = "There was an error deleting the purchase."
+      render :show
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_purchase
       @purchase = Purchase.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def purchase_params
-      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id)
+      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id, :price)
     end
 end
+
+
+#class Invoices::PurchasesController < ApplicationController
+
+
+  #def index
+   # @purchases = Purchase.all
+   # respond_with(@purchases)
+ # end
+
+ # def show
+   # respond_with(@purchase)
+ # end
+
+ # def new
+  #  @invoice = Invoice.find(params[:invoice_id] )
+   # @purchase = Purchase.new
+ # end
+
+  #def edit
+ # end
+
+  #def create
+  #  @invoice = Invoice.find(params[:invoice_id])
+   # @purchase = Purchase.new(purchase_params)
+   # @purchase.invoice = @invoice
+   # @purchase.save
+    #respond_with(@purchase)
+#  end
+
+ # def update
+   # @purchase.update(purchase_params)
+   # respond_with(@purchase)
+  #end
+
+  #def destroy
+  #  @purchase.destroy
+  #  respond_with(@purchase)
+  #end
+
+ # private
+   # def set_purchase
+    #  @purchase = Purchase.find(params[:id])
+  #  end
+
+   # def purchase_params
+    #  params.require(:purchase).permit(:name, :category, :quantity, :invoice_id)
+   # end
+#end
