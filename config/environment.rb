01ff028b21a7b838af6746a2579f2207af729c3c
Roy Bakker <roybakker17@gmail.com>
2015-01-28 11:47:06 +0000

    Heroku 3

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => 'enterpriseape-roy.herokuapp.com' }
+
 # Initialize the Rails application.
 Enterpriseape::Application.initialize!
